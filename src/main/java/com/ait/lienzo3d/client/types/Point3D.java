a9237206282fea100208840b4d8dd8cc4e9cd21f
deanjones <deanjones@gmail.com>
2014-08-29 16:53:17 -0400

    #1: add 3D types

---
+++ src/main/java/com/ait/lienzo3d/client/types/Point3D.java
@@ -0,0 +1,340 @@
+/*
+   Copyright (c) 2014 Ahome' Innovation Technologies. All rights reserved.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+package com.ait.lienzo3d.client.types;
+
+import com.ait.lienzo.client.core.util.GeometryException;
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.json.client.JSONObject;
+
+public final class Point3D
+{
+    private final Point3DJSO m_jso;
+
+    public Point3D(Point3DJSO jso)
+    {
+        m_jso = jso;
+    }
+
+    public Point3D()
+    {
+        this(Point3DJSO.make(0, 0, 0));
+    }
+
+    public Point3D(double x, double y, double z)
+    {
+        this(Point3DJSO.make(x, y, z));
+    }
+
+    /**
+     * Returns the x coordinate
+     * @return double
+     */
+    public final double getX()
+    {
+        return m_jso.getX();
+    }
+
+    /**
+     * Sets the x coordinate
+     * @param x double
+     * @return this Point3D
+     */
+    public final Point3D setX(double x)
+    {
+        m_jso.setX(x);
+
+        return this;
+    }
+
+    /**
+     * Returns the y coordinate
+     * @return double
+     */
+    public final double getY()
+    {
+        return m_jso.getY();
+    }
+
+    /**
+     * Sets the y coordinate
+     * @param y double
+     * @return this Point3D
+     */
+    public final Point3D setY(double y)
+    {
+        m_jso.setY(y);
+
+        return this;
+    }
+
+    /**
+     * Returns the z coordinate
+     * @return double
+     */
+    public final double getZ()
+    {
+        return m_jso.getZ();
+    }
+
+    /**
+     * Sets the z coordinate
+     * @param z double
+     * @return this Point3D
+     */
+    public final Point3D setZ(double z)
+    {
+        m_jso.setZ(z);
+
+        return this;
+    }
+
+    public final Point3D set(Point3D p)
+    {
+        m_jso.setX(p.getX());
+
+        m_jso.setY(p.getY());
+
+        m_jso.setZ(p.getZ());
+
+        return this;
+    }
+
+    public final double getLength()
+    {
+        return m_jso.getLength();
+    };
+
+    public final double distance(Point3D other)
+    {
+        return m_jso.distance(other.getJSO());
+    };
+
+    public static final double distance(Point3D a, Point3D b)
+    {
+        return Point3DJSO.distance(a.getJSO(), b.getJSO());
+    };
+
+    public final Point3D plus(Point3D p)
+    {
+        return new Point3D(m_jso.plus(p.getJSO()));
+    }
+
+    public final Point3D minus(Point3D p)
+    {
+        return new Point3D(m_jso.minus(p.getJSO()));
+    }
+
+    public final Point3D div(double d) throws GeometryException
+    {
+        if (d == 0.0)
+        {
+            throw new GeometryException("can't divide by 0");
+        }
+        return scale(1.0 / d);
+    }
+
+    public final Point3D scale(double d)
+    {
+        return new Point3D(m_jso.scale(d));
+    }
+
+    public final Point3D unit() throws GeometryException
+    {
+        double len = getLength();
+
+        if (len == 0)
+        {
+            throw new GeometryException("can't normalize (0,0,0)");
+        }
+        return div(len);
+    }
+
+    public final double thetaTo(Point3D p)
+    {
+        return m_jso.thetaTo(p.getJSO());
+    }
+
+    public final Point3D copy()
+    {
+        return new Point3D(m_jso.copy());
+    }
+
+    public final double dot(Point3D p)
+    {
+        return m_jso.dot(p.getJSO());
+    }
+
+    public final Point3D cross(Point3D p)
+    {
+        return new Point3D(m_jso.cross(p.getJSO()));
+    }
+
+    @Override
+    public final String toString()
+    {
+        return new JSONObject(getJSO()).toString();
+    }
+
+    public final Point3DJSO getJSO()
+    {
+        return m_jso;
+    }
+
+    public static class Point3DJSO extends JavaScriptObject
+    {
+        protected Point3DJSO()
+        {
+        }
+
+        public static native Point3DJSO make(double xval, double yval, double zval)
+        /*-{
+			return {
+				x : xval,
+				y : yval,
+				z : zval
+			};
+        }-*/;
+
+        public final native static double distance(Point3DJSO a, Point3DJSO b)
+        /*-{
+			var dx = b.x - a.x;
+
+			var dy = b.y - a.y;
+
+			var dz = b.z - a.z;
+
+			return Math.sqrt((dx * dx) + (dy * dy) * (dz * dz));
+        }-*/;
+
+        public final native static double length(Point3DJSO a)
+        /*-{
+			var dx = a.x;
+
+			var dy = a.y;
+
+			var dz = a.z;
+
+			return Math.sqrt((dx * dx) + (dy * dy) + (dz * dz));
+        }-*/;
+
+        public native Point3DJSO copy()
+        /*-{
+			return {
+				x : this.x,
+				y : this.y,
+				z : this.z
+			};
+        }-*/;
+
+        public final native double dot(Point3DJSO p)
+        /*-{
+			return this.x * p.x + this.y * p.y + this.z * p.z;
+        }-*/;
+
+        public final double thetaTo(Point3DJSO p)
+        {
+            return thetaTo_(p, dot(p));
+        }
+
+        private final native double thetaTo_(Point3DJSO p, double dot)
+        /*-{
+			var dx = this.y * p.z - this.z * p.y;
+
+			var dy = this.z * p.x - this.x * p.z;
+
+			var dz = this.x * p.y - this.y * p.x;
+
+			return Math.atan2(Math.sqrt(dx * dx + dy * dy + dz * dz), dot);
+        }-*/;
+
+        private final native Point3DJSO cross(Point3DJSO p)
+        /*-{
+			return {
+				x : this.y * p.z - this.z * p.y,
+				y : this.z * p.x - this.x * p.z,
+				z : this.x * p.y - this.y * p.x
+			};
+        }-*/;
+
+        public final native double getX()
+        /*-{
+			return this.x;
+        }-*/;
+
+        public final native void setX(double x)
+        /*-{
+			this.x = x;
+        }-*/;
+
+        public final native double getY()
+        /*-{
+			return this.y;
+        }-*/;
+
+        public final native void setY(double y)
+        /*-{
+			this.y = y;
+        }-*/;
+
+        public final native double getZ()
+        /*-{
+			return this.z;
+        }-*/;
+
+        public final native void setZ(double z)
+        /*-{
+			this.z = z;
+        }-*/;
+
+        public final double distance(Point3DJSO other)
+        {
+            return distance(this, other);
+        };
+
+        public final double getLength()
+        {
+            return length(this);
+        };
+
+        public final native Point3DJSO plus(Point3DJSO jso)
+        /*-{
+			return {
+				x : this.x + jso.x,
+				y : this.y + jso.y,
+				z : this.z + jso.z
+			};
+        }-*/;
+
+        public final native Point3DJSO minus(Point3DJSO jso)
+        /*-{
+			return {
+				x : this.x - jso.x,
+				y : this.y - jso.y,
+				z : this.z - jso.z
+			};
+        }-*/;
+
+        public final native Point3DJSO scale(double d)
+        /*-{
+			return {
+				x : this.x * d,
+				y : this.y * d,
+				z : this.z * d
+			};
+        }-*/;
+    }
+}
---
+++ src/main/java/com/ait/lienzo3d/client/types/Point3DArray.java
@@ -0,0 +1,209 @@
+/*
+   Copyright (c) 2014 Ahome' Innovation Technologies. All rights reserved.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+package com.ait.lienzo3d.client.types;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+
+import com.ait.lienzo3d.client.types.Point3D.Point3DJSO;
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.core.client.JsArray;
+import com.google.gwt.json.client.JSONArray;
+
+/**
+ * Point3DArray represents an array (or List) with {@link Point3D} objects.
+ */
+public class Point3DArray implements Iterable<Point3D>
+{
+    private final Point3DArrayJSO m_jso;
+
+    public Point3DArray(Point3DArrayJSO jso)
+    {
+        m_jso = jso;
+    }
+
+    public Point3DArray(JsArray<JavaScriptObject> jso)
+    {
+        m_jso = jso.cast();
+    }
+
+    public Point3DArray()
+    {
+        this(Point3DArrayJSO.makePoint3DArrayJSO());
+    }
+
+    public Point3DArray(double x, double y)
+    {
+        this(Point3DArrayJSO.makePoint3DArrayJSO());
+    }
+
+    public Point3DArray(Point3D point)
+    {
+        this(Point3DArrayJSO.makePoint3DArrayJSO());
+
+        push(point);
+    }
+
+    public Point3DArray(Point3D point, Point3D... points)
+    {
+        this(Point3DArrayJSO.makePoint3DArrayJSO());
+
+        push(point, points);
+    }
+
+    public Point3DArray(double[] x, double[] y, double[] z)
+    {
+        this(Point3DArrayJSO.makePoint3DArrayJSO());
+
+        if (x.length != y.length)
+        {
+            throw new IllegalArgumentException("x and y array should have the same length");
+        }
+        if (x.length != z.length)
+        {
+            throw new IllegalArgumentException("x and z array should have the same length");
+        }
+        for (int i = 0; i < x.length; i++)
+        {
+            push(x[i], y[i], z[i]);
+        }
+    }
+
+    public Point3DArray(double[][] points)
+    {
+        this(Point3DArrayJSO.makePoint3DArrayJSO());
+
+        for (int i = 0; i < points.length; i++)
+        {
+            double[] xyz = points[i];
+
+            if (xyz.length != 3)
+            {
+                throw new IllegalArgumentException("points[" + i + "] does not have length of 3");
+            }
+            push(xyz[0], xyz[1], xyz[2]);
+        }
+    }
+
+    public final Point3DArray push(Point3D point)
+    {
+        getJSO().push(point.getJSO());
+
+        return this;
+    }
+
+    public final Point3DArray push(double x, double y, double z)
+    {
+        getJSO().push(Point3DJSO.make(x, y, z));
+
+        return this;
+    }
+
+    public final Point3DArray push(Point3D point, Point3D... points)
+    {
+        getJSO().push(point.getJSO());
+
+        if (points != null)
+        {
+            for (int i = 0; i < points.length; i++)
+            {
+                getJSO().push(points[i].getJSO());
+            }
+        }
+        return this;
+    }
+
+    public final int size()
+    {
+        return getJSO().length();
+    }
+
+    public final Point3D getPoint(int i)
+    {
+        return new Point3D(getJSO().get(i));
+    }
+
+    public final Point3DArray setPoint(int i, Point3D p)
+    {
+        getJSO().set(i, p.getJSO());
+
+        return this;
+    }
+
+    public Point3DArray shift()
+    {
+        m_jso.shift();
+
+        return this;
+    }
+
+    public Point3DArray pop()
+    {
+        m_jso.pop();
+
+        return this;
+    }
+
+    public final Collection<Point3D> getPoints()
+    {
+        int leng = size();
+
+        ArrayList<Point3D> list = new ArrayList<Point3D>(leng);
+
+        for (int i = 0; i < leng; i++)
+        {
+            list.add(getPoint(i));
+        }
+        return Collections.unmodifiableCollection(list);
+    }
+
+    @Override
+    public String toString()
+    {
+        return new JSONArray(getJSO()).toString();
+    }
+
+    @Override
+    public Iterator<Point3D> iterator()
+    {
+        return getPoints().iterator();
+    }
+
+    public final Point3DArrayJSO getJSO()
+    {
+        return m_jso;
+    }
+
+    public static final class Point3DArrayJSO extends JsArray<Point3DJSO>
+    {
+        protected Point3DArrayJSO()
+        {
+        }
+
+        public final native void pop()
+        /*-{
+			this.pop();
+        }-*/;
+
+        public static final native Point3DArrayJSO makePoint3DArrayJSO()
+        /*-{
+			return [];
+        }-*/;
+    }
+}
